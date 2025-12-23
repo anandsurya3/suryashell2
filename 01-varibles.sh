66fedefc41cb3695336cb866c78b950f3e7c91dc
Anand <kallaanand0@gmail.com>
2025-12-23 14:08:16 +0530

    01-varibles.sh

---
+++ 01-varibles.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "nandu : hi devy .. how are you "
+echo "devy : hi nandu .. iam fine , how are you "
+eccho "nandu : iam good, did you buy the car devy"
+echo "devy : no nandu i didn't purchase"
+
+
