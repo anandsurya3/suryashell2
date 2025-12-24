9915285615f1d246b9ffec76149d79a39fb38a2d
Anand <kallaanand0@gmail.com>
2025-12-24 15:55:44 +0530

    somevariables

--- newvari.sh
+++ newvari.sh
@@ -3,3 +3,5 @@ echo "ambedkar name is; $ambedkar"
 echo "rsagarawal name is; $rsagawaral"
 echo "rsagarawal name is: $rsagarawal"
 echo "i wanto make it or else destroy it is as soon as possbile"
+
+echo "samsung name is: $samsung"
