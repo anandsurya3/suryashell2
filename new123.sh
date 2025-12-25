8ba0831200fbb4cf660e6349f12769829dc312e1
Anand <kallaanand0@gmail.com>
2025-12-25 09:34:34 +0530

    some

--- new123.sh
+++ new123.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+#!/b$THING1n/bash
 
+THING1=i
 
 
 PERSON4=$1
 PERSON3=$2
 
-echo $PERSON3: "Hi there, it is $PERSON4, right?"
-echo $PERSON4: "Hi, yes! You must be $PERSON3. You recently joined the team, correct?"
-echo $PERSON3: "Yes indeed, $PERSON4. The group manager told me that you would be my mentor for the first few months."
-echo $PERSON4: "That’s right, $PERSON3. I'll be training you on the digital marketing module. Do you have any prior experience, $PERSON3?"
-echo $PERSON3: "I have basic SEO knowledge, $PERSON4, but I'm keen to expand into a specialist role."
-echo $PERSON4: "Awesome, $PERSON3. I’ve been in this domain for six years, so I should be able to guide you well."
-echo $PERSON3: "Thanks a lot, $PERSON4. Since it’s my first day, are there any other formalities I should know about?"
-echo $PERSON4: "You should be fine, $PERSON3. Just make sure your name is in the transport roster if you use the shuttle."
-echo $PERSON3: "Oh, thanks for informing me, $PERSON4. I will check that during the break."
-echo $PERSON4: "No problem, $PERSON3. Let's head to the meeting now."
+echo $PERSON3: "H$THING1 there, $THING1t $THING1s $PERSON4, r$THING1ght?"
+echo $PERSON4: "H$THING1, yes! You must be $PERSON3. You recently jo$THING1ned the team, correct?"
+echo $PERSON3: "Yes $THING1ndeed, $PERSON4. The group manager told me that you would be my mentor for the f$THING1rst few months."
+echo $PERSON4: "That’s r$THING1ght, $PERSON3. $THING1'll be tra$THING1n$THING1ng you on the d$THING1g$THING1tal market$THING1ng module. Do you have any pr$THING1or exper$THING1ence, $PERSON3?"
+echo $PERSON3: "$THING1 have bas$THING1c SEO knowledge, $PERSON4, but $THING1'm keen to expand $THING1nto a spec$THING1al$THING1st role."
+echo $PERSON4: "Awesome, $PERSON3. $THING1’ve been $THING1n th$THING1s doma$THING1n for s$THING1x years, so $THING1 should be able to gu$THING1de you well."
+echo $PERSON3: "Thanks a lot, $PERSON4. S$THING1nce $THING1t’s my f$THING1rst day, are there any other formal$THING1t$THING1es $THING1 should know about?"
+echo $PERSON4: "You should be f$THING1ne, $PERSON3. Just make sure your name $THING1s $THING1n the transport roster $THING1f you use the shuttle."
+echo $PERSON3: "Oh, thanks for $THING1nform$THING1ng me, $PERSON4. $THING1 w$THING1ll check that dur$THING1ng the break."
+echo $PERSON4: "No problem, $PERSON3. Let's head to the meet$THING1ng now."
