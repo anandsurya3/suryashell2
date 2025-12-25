49802182c53420e74174a2ac232bdc9f005017f1
Anand <kallaanand0@gmail.com>
2025-12-25 09:23:54 +0530

    some

---
+++ new123.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+PERSON4=$1
+PERSON3=$2
+
+$PERSON3: "Hi there, it is $PERSON4, right?"
+$PERSON4: "Hi, yes! You must be $PERSON3. You recently joined the team, correct?"
+$PERSON3: "Yes indeed, $PERSON4. The group manager told me that you would be my mentor for the first few months."
+$PERSON4: "That’s right, $PERSON3. I'll be training you on the digital marketing module. Do you have any prior experience, $PERSON3?"
+$PERSON3: "I have basic SEO knowledge, $PERSON4, but I'm keen to expand into a specialist role."
+$PERSON4: "Awesome, $PERSON3. I’ve been in this domain for six years, so I should be able to guide you well."
+$PERSON3: "Thanks a lot, $PERSON4. Since it’s my first day, are there any other formalities I should know about?"
+$PERSON4: "You should be fine, $PERSON3. Just make sure your name is in the transport roster if you use the shuttle."
+$PERSON3: "Oh, thanks for informing me, $PERSON4. I will check that during the break."
+$PERSON4: "No problem, $PERSON3. Let's head to the meeting now."
